f68105bcb8b9007be582b3c16576118fd73162c6
luc <luclammers06@gmail.com>
2024-05-26 16:45:16 +0200

    feat: Update QR code encryption and decryption
    This commit updates the encryption and decryption logic for QR codes in the `checkQR` and `generateQR` functions. It replaces the previous implementation using JWT with a new implementation using AES-256-CBC encryption. The QR code data is now encrypted using a randomly generated initialization vector (IV) and the secret key. This change improves the security of the QR codes and ensures that the data can only be decrypted by authorized parties.

--- backend/src/services/qrcode.ts
+++ backend/src/services/qrcode.ts
@@ -1,22 +1,47 @@
-import jwt from 'jsonwebtoken';
+import crypto from 'crypto';
 import qrcode from 'qrcode';
 
+const algorithm = 'aes-256-cbc';
 const secret = process.env.QR_SECRET!;
-const expiry = process.env.QR_EXPIRY!;
+const iv = crypto.randomBytes(16);
 
-type Token = {
+const expiry = 35 * 1000;
+
+type Encrypted = {
+    iv: string;
+    encryptedData: string
+};
+
+type Data = {
     id: number;
     type: 'ticket' | 'wallet';
-    iat: number;
     exp: number;
 };
 
-export const checkQR = async (qrCode: string) => {
-    try {
-        return jwt.verify(qrCode, secret) as Token;
-    } catch {
-        return false;
-    }
+export const checkQR = async (qr: string) => {
+
+    const encrypted = await JSON.parse(qr) as Encrypted;
+
+    const decipher = crypto.createDecipheriv(algorithm, Buffer.from(secret), Buffer.from(encrypted.iv, 'hex'));
+
+    let decrypted = decipher.update(Buffer.from(encrypted.encryptedData, 'hex'));
+    decrypted = Buffer.concat([decrypted, decipher.final()]);
+
+    const data = await JSON.parse(decrypted.toString()) as Data;
+
+    if (data.exp < Date.now()) return false;
+
+    return data;
 };
 
-export const generateQR = async (id: number, type: string) => await qrcode.toDataURL(jwt.sign({ id, type }, secret, { expiresIn: expiry }));
+export const generateQR = async (id: number, type: string) => {
+    const text = JSON.stringify({ id, type, exp: Date.now() + expiry });
+    
+    const cipher = crypto.createCipheriv(algorithm, Buffer.from(secret), iv);
+
+    let encrypted = cipher.update(text);
+    encrypted = Buffer.concat([encrypted, cipher.final()]);
+
+
+    return await qrcode.toDataURL(JSON.stringify({ iv: iv.toString('hex'), encryptedData: encrypted.toString('hex') }));
+}
